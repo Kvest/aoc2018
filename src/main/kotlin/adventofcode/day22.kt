6f0a713940791da9f30e55a2895b5743e0973242
Roman Bielokon <belokon.roman@gmail.com>
2019-01-23 22:43:44 +0100

    improved day 22 solution

--- src/main/kotlin/adventofcode/day22.kt
+++ src/main/kotlin/adventofcode/day22.kt
@@ -1,21 +1,28 @@
 package adventofcode
 
 import java.util.*
-import kotlin.math.roundToInt
 
 fun main(args: Array<String>) {
     first22(14, 778, 11541)
     second22(14, 778, 11541)
-    //second22(10, 10, 510)
+
+    //first22(10, 725, 8787)
+    //second22(10, 725, 8787)
 }
 
 private val X_COEFF = 16807
 private val Y_COEFF = 48271
 private val MODE = 20183
 private val CHANGE_EQUIPMENT_TIME = 7
+private val MOVE_TIME = 1
+
 private val ROCK = 0
 private val WET = 1
 private val NARROW = 2
+
+private val TORCH = 0
+private val GEAR = 1
+private val NOTHING = 2
 
 private fun first22(targetX: Int, targetY: Int, depth: Int) {
     val cave = buildCave(targetX, targetY, depth)
@@ -26,77 +33,80 @@ private fun first22(targetX: Int, targetY: Int, depth: Int) {
 
     }
     println(riskLevel)
-
-//    println(el.joinToString(separator = "\n") {
-//        it.joinToString(separator = "") {
-//            when (it % 3) {
-//                0 -> "."
-//                1 -> "="
-//                2 -> "|"
-//                else -> "?"
-//            }
-//        }
-//    })
 }
 
 private fun second22(targetX: Int, targetY: Int, depth: Int) {
+    val changeEq = mapOf(
+            XY(ROCK, TORCH) to GEAR,
+            XY(ROCK, GEAR) to TORCH,
+
+            XY(WET, GEAR) to NOTHING,
+            XY(WET, NOTHING) to GEAR,
+
+            XY(NARROW, TORCH) to NOTHING,
+            XY(NARROW, NOTHING) to TORCH
+    )
     val transitions = mapOf(
-            XYZ(ROCK, 1, WET) to 0,
-            XYZ(ROCK, 0, NARROW) to 0,
-            XYZ(WET, 1, NARROW) to 1,
-            XYZ(WET, 0, ROCK) to 1,
-            XYZ(NARROW, 0, ROCK) to 0,
-            XYZ(NARROW, 1, WET) to 1
+            XYZ(ROCK, GEAR, WET) to GEAR,
+            XYZ(ROCK, TORCH, NARROW) to TORCH,
+
+            XYZ(WET, NOTHING, NARROW) to NOTHING,
+            XYZ(WET, GEAR, ROCK) to GEAR,
+
+            XYZ(NARROW, TORCH, ROCK) to TORCH,
+            XYZ(NARROW, NOTHING, WET) to NOTHING
     )
 
     val w = (targetX + 1) * 100
     val h = (targetY + 1) * 10
     val cave = buildCave(w, h, depth)
+    cave[targetX][targetY] = ROCK
 
-    val dist = Array(h) {
-        Array(w) {
-            IntArray(2) { Int.MAX_VALUE }
+    val dist = Array(w) {
+        Array(h) {
+            IntArray(3) { Int.MAX_VALUE }
         }
     }
 
-    val comparator = Comparator<XYZ> { first, second -> dist[first.y][first.x][first.z] - dist[second.y][second.x][second.z] }
+    val comparator = Comparator<XYZ> { first, second -> dist[first.x][first.y][first.z] - dist[second.x][second.y][second.z] }
     val queue = PriorityQueue<XYZ>(comparator)
-    dist[0][0][0] = 0
-    queue.add(XYZ(0, 0, 0))
+    dist[0][0][TORCH] = 0
+    queue.add(XYZ(0, 0, TORCH))
 
     while (queue.isNotEmpty()) {
         val next = queue.poll()
-        if (next.x == targetX && next.y == targetY && next.z == 0) {
+        if (next.x == targetX && next.y == targetY && next.z == TORCH) {
             break
         }
 
-        //fill sell for changed equipment
-        val opposite = XYZ(next.x, next.y, if (next.z == 0) 1 else 0)
-        if (dist[next.y][next.x][next.z] + CHANGE_EQUIPMENT_TIME < dist[opposite.y][opposite.x][opposite.z]) {
-            queue.removeAll(opposite)
+        val curr = cave[next.x][next.y]
 
-            dist[opposite.y][opposite.x][opposite.z] = dist[next.y][next.x][next.z] + CHANGE_EQUIPMENT_TIME
-            queue.add(opposite)
+        //changed equipment
+        val ch = XYZ(next.x, next.y, changeEq[XY(curr, next.z)]!!)
+        if (dist[next.x][next.y][next.z] + CHANGE_EQUIPMENT_TIME < dist[ch.x][ch.y][ch.z]) {
+            queue.removeAll(ch)
+
+            dist[ch.x][ch.y][ch.z] = dist[next.x][next.y][next.z] + CHANGE_EQUIPMENT_TIME
+            queue.add(ch)
         }
 
         //check neighbors
-        val curr = cave[next.y][next.x]
-        neighbors(next.y, next.x) { i, j ->
-            //         println("$next  = $curr [$i,$j] comp ${cave[i][j]}")
-            if (cave[i][j] == curr && dist[next.y][next.x][next.z] + 1 < dist[i][j][next.z]) {
-                val opposite = XYZ(j, i, next.z)
+        neighbors(next.x, next.y) { x, y ->
+            //if neighbor is the same type
+            if (cave[x][y] == curr && dist[next.x][next.y][next.z] + MOVE_TIME < dist[x][y][next.z]) {
+                val opposite = XYZ(x, y, next.z)
                 queue.removeAll(opposite)
 
-                dist[opposite.y][opposite.x][opposite.z] = dist[next.y][next.x][next.z] + 1
+                dist[opposite.x][opposite.y][opposite.z] = dist[next.x][next.y][next.z] + MOVE_TIME
                 queue.add(opposite)
             } else {
-                val newZ = transitions[XYZ(curr, next.z, cave[i][j])]
+                val newZ = transitions[XYZ(curr, next.z, cave[x][y])]
                 newZ?.let { newZ ->
-                    if (dist[next.y][next.x][next.z] + 1 < dist[i][j][newZ]) {
-                        val opposite = XYZ(j, i, newZ)
+                    if (dist[next.x][next.y][next.z] + MOVE_TIME < dist[x][y][newZ]) {
+                        val opposite = XYZ(x, y, newZ)
                         queue.removeAll(opposite)
 
-                        dist[opposite.y][opposite.x][opposite.z] = dist[next.y][next.x][next.z] + 1
+                        dist[opposite.x][opposite.y][opposite.z] = dist[next.x][next.y][next.z] + MOVE_TIME
                         queue.add(opposite)
                     }
                 }
@@ -104,42 +114,42 @@ private fun second22(targetX: Int, targetY: Int, depth: Int) {
         }
     }
 
-    println("finished ${dist[targetY][targetX][0]} ${dist[targetY][targetX][1]}")
+    println("finished ${dist[targetX][targetY][TORCH]}")
 }
 
-private fun PriorityQueue<*>.removeAll(item: Any) {
-    while (this.remove(item)) {
+private fun PriorityQueue<XYZ>.removeAll(xyz: XYZ) {
+    while (this.remove(xyz)) {
     }
 }
 
 private fun buildCave(targetX: Int, targetY: Int, depth: Int): Array<IntArray> {
-    val el = Array(targetY + 1) { IntArray(targetX + 1) { 0 } }
-    el.forEachIndexed { y, x, _ ->
+    val el = Array(targetX + 1) { IntArray(targetY + 1) { 0 } }
+    el.forEachIndexed { x, y, _ ->
         if ((y == 0 && x == 0) || (y == targetY && x == targetX)) {
-            el[y][x] = 0
+            el[x][y] = 0
         } else if (y == 0) {
-            el[y][x] = (x * X_COEFF + depth) % MODE
+            el[x][y] = (x * X_COEFF + depth) % MODE
         } else if (x == 0) {
-            el[y][x] = (y * Y_COEFF + depth) % MODE
+            el[x][y] = (y * Y_COEFF + depth) % MODE
         } else {
-            el[y][x] = (el[y][x - 1] * el[y - 1][x] + depth) % MODE
+            el[x][y] = (el[x - 1][y] * el[x][y - 1] + depth) % MODE
         }
     }
 
-    el.forEachIndexed { y, x, _ ->
-        el[y][x] = el[y][x] % 3
+    el.forEachIndexed { x, y, _ ->
+        el[x][y] = el[x][y] % 3
     }
 
     return el
 }
 
-inline fun neighbors(i: Int, j: Int, action: (Int, Int) -> Unit) {
-    if (i > 0) {
-        action(i - 1, j)
+private inline fun neighbors(x: Int, y: Int, action: (Int, Int) -> Unit) {
+    if (x > 0) {
+        action(x - 1, y)
     }
-    if (j > 0) {
-        action(i, j - 1)
+    if (y > 0) {
+        action(x, y - 1)
     }
-    action(i, j + 1)
-    action(i + 1, j)
+    action(x, y + 1)
+    action(x + 1, y)
 }
